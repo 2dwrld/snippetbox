ef1b351ebe986762dbf487620aa79cef47e81fa2
2dwrld <aintstressedanymore@gmail.com>
2024-10-16 20:32:59 +0300

    HTML templating and inheritance

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"fmt"
+   "log"
 	"io"
+   "html/template"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello from Snippetbox")
+   w.Header().Add("Server", "Go")
+
+   files := []string {
+      "./ui/html/base.tmpl",
+      "./ui/html/partials/nav.tmpl",
+      "./ui/html/pages/home.tmpl",
+   }
+
+   ts, err := template.ParseFiles(files...)
+   if err != nil {
+      log.Print(err.Error())
+      http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+      return
+   }
+
+   err = ts.ExecuteTemplate(w, "base", nil)
+   if err != nil {
+      log.Print(err.Error())
+      http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+   }
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
+   w.Header().Add("Server", "Go")
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -21,6 +43,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+   w.Header().Add("Server", "Go")
 	io.WriteString(w, "Display a form for creating a new snippet...")
 }
 
