346451f7ed25f24790eeb8bddf7ebc30eb893af2
2dwrld <aintstressedanymore@gmail.com>
2024-10-17 21:35:47 +0300

    Dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-   "log"
 	"io"
    "html/template"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
    w.Header().Add("Server", "Go")
 
    files := []string {
@@ -20,19 +19,19 @@ func home(w http.ResponseWriter, r *http.Request) {
 
    ts, err := template.ParseFiles(files...)
    if err != nil {
-      log.Print(err.Error())
+      app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.RequestURI())
       http.Error(w, "Internal Server Error", http.StatusInternalServerError)
       return
    }
 
    err = ts.ExecuteTemplate(w, "base", nil)
    if err != nil {
-      log.Print(err.Error())
+      app.logger.Error(err.Error(), "method", r.Method, "url", r.URL.RequestURI())
       http.Error(w, "Internal Server Error", http.StatusInternalServerError)
    }
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
    w.Header().Add("Server", "Go")
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
@@ -42,12 +41,12 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
    w.Header().Add("Server", "Go")
 	io.WriteString(w, "Display a form for creating a new snippet...")
 }
 
-func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
    w.Header().Add("Server", "Go")
    w.WriteHeader(http.StatusCreated)
    io.WriteString(w, "Save a new snippet...")
