42faa7f15cf8822e00c9848c52711af38c05476c
2dwrld <aintstressedanymore@gmail.com>
2024-10-17 21:51:41 +0300

    Isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,34 +8,25 @@ import (
 )
 
 type application struct {
-   logger *slog.Logger
+	logger *slog.Logger
 }
 
 func main() {
-   addr := flag.String("addr", ":4000", "HTTP network address")
-   flag.Parse()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 
-   logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-      AddSource: true,
-      Level: slog.LevelInfo,
-   }))
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+	}))
 
-   app := &application {
-      logger: logger,
-   }
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	app := &application{
+		logger: logger,
+	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
-   os.Exit(1)
+	os.Exit(1)
 }
